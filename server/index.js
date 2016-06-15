6862c5524b6e4c4d7a675275175bfe946547cfbe
tommy.crafton <tommy.crafton@ruralsourcing.com>
2016-06-15 15:35:03 -0500

    Added code to handle cors issue from client server

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // DB Setup
 //mongoose.connect('mongodb://localhost:auth/auth');
@@ -14,6 +15,7 @@ mongoose.connect('mongodb://twc:testpass@ds015574.mlab.com:15574/auth');
 // App Setup
 // morgan is for logging
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*'}));
 router(app);
 
