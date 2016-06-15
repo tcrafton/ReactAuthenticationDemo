c1e6d4287f2b5778c14974edd49511418810c794
tommy.crafton <tommy.crafton@ruralsourcing.com>
2016-06-15 12:22:15 -0500

    Finished code for server-side authentication

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,18 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user){
+  const timestamp = new Date().getTime();
+  // sub = subject, iat = issued at time
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signin = function(req, res, next){
+  // User has already had their email and password auth'd
+  // We just need to give them a token
+  res.send({ token: tokenForUser(req.user) });
+}
 
 exports.signup = function(req, res, next) {
   const email = req.body.email;
@@ -28,7 +42,7 @@ exports.signup = function(req, res, next) {
       if(err) {return next(err); }
 
       // Respond to request indicating the user was created
-      res.json({ success: true });
+      res.json({ token: tokenForUser(user) });
     });
   });
 
